f7b4ee281bcc61e3621c3c42c3568485fbffeab7
joao-evaristo <joao.evaristojw@gmail.com>
2024-04-07 20:47:09 -0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'sessions#new'
+  get 'sessions/new'
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'sign_in' => 'sessions#new'
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_out' => 'sessions#destroy'
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
